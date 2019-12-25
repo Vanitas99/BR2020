bb4ce5fc7c0ea4ead536bab0e4d1944781376420
Vanitas99 <36307377+Vanitas99@users.noreply.github.com>
2019-12-25 20:09:24 +0100

    Rough PlayerMovement and Simple Ray Detection

---
+++ BR19_20/Assets/HitDetection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDetection : MonoBehaviour
+{
+    Vector3 playerPosition;
+    Vector3 lightPosition;
+    public bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightPosition = GameObject.Find("MainLightSource").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = gameObject.transform.position + (Vector3.down * (transform.localScale.y));
+
+        if(isPlayerGrounded()) 
+        {
+            isGrounded = true;
+            //Debug.Log("Player is grounded");
+            //Debug.DrawRay(playerPosition, lightPosition - playerPosition, Color.red);
+        } else {
+            isGrounded = false;
+            //Debug.Log("Player in Air");
+            //Debug.DrawRay(playerPosition, lightPosition - playerPosition);
+        } 
+    }
+
+    private bool isPlayerGrounded() {
+        return (Physics.Raycast(playerPosition, lightPosition - playerPosition));
+    }
+}
---
+++ BR19_20/Assets/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    HitDetection hitDetection;
+    Rigidbody rigidbody;
+
+    Vector3 m_Velocity = Vector3.zero;
+    [SerializeField] private float m_SmoothStep = .05f;
+    [SerializeField] private float jumpForce = 2f;
+
+    [SerializeField] private bool jump;
+    [SerializeField] private float moveSpeed = 2f;
+
+    [SerializeField] private bool grounded;
+
+    float dir;
+
+    private void Awake() {
+        //Application.targetFrameRate = 60; 
+        //QualitySettings.vSyncCount = 1;   
+    }
+
+    void Start()
+    {
+        Physics.gravity = new Vector3(0,-5,0);
+        hitDetection = GetComponent<HitDetection>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        checkIfGrounded();
+        handleInput();
+        
+    }
+
+    private void FixedUpdate() 
+    {
+        move(dir, false, jump);
+    }
+    private void checkIfGrounded() {
+        if(hitDetection.isGrounded) {
+            grounded = true;
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x,0f,rigidbody.velocity.z);
+            Physics.gravity = Vector3.zero;
+        } else {
+            grounded = false;
+            Physics.gravity = new Vector3(0,-5f,0);
+        }
+    }
+
+    private void move(float dir, bool crouch, bool jump)
+    {
+        Vector3 targetVelocity = new Vector3(dir*moveSpeed, rigidbody.velocity.y, rigidbody.velocity.z);
+
+        rigidbody.velocity = Vector3.SmoothDamp(rigidbody.velocity, targetVelocity,ref m_Velocity, m_SmoothStep);
+
+        if (jump) {
+            rigidbody.AddForce(new Vector3(0,jumpForce,0));
+        }
+    }
+
+    private void handleInput() 
+    {
+        dir = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.W)) {
+            jump = true;
+        } else {
+            jump = false;
+        }
+    }
+}
